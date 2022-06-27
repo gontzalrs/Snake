3b4d5ffebdeab2c4c24eb07ec2e93907746637d8
Gontzal Ruiz <gontzalruizs@gmail.com>
2022-06-27 16:35:53 +0200

    commit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 
 from copy import deepcopy
 
-def main():
-    print("numberOfAvailableDifferentPaths = ")    
-    print(numberOfAvailableDifferentPaths([2, 3], [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]], 10))
 
 def numberOfAvailableDifferentPaths(board, snake, depth):
     """Returns the available number of Paths for the given snake on the given board, 
@@ -49,11 +46,28 @@ def sumAvailableDifferentPaths(board, snake, depth):
     downSum = 0
     if tryDown(board[0]-1, snake) == True:
         newSnake = [snake[0].copy()] + deepcopy(snake)[:-1]
-        newSnake[0][0] = newSnake[0][0]-1
+        newSnake[0][0] = newSnake[0][0]+1
         downSum = sumAvailableDifferentPaths(board, newSnake, depth-1)
     
     return leftSum+upSum+rightSum+downSum
 
+def selfIntersects(snake, nextHeadPosition):
+    """Checks whether the snake would collide with itself or not.
+
+    Parameters
+    ----------
+    snake : array of 2-dimensional arrays 
+        Position of each snake bodypart on the board.
+        3 <= snake length <= 7
+    nextHeadPosition : 2-dimensional array of integers
+        Position the head of the snake is attempting to take.
+    """
+
+    for bodyPartPosition in snake[1:-1]:
+        if nextHeadPosition == bodyPartPosition:
+            return True
+    
+    return False
 
 def tryLeft(snake):
     """Try of a movement to the left by the snake head. If it can be done, 
@@ -131,21 +145,6 @@ def tryDown(lastRowIndex, snake):
     nextHeadPosition[0] = nextHeadPosition[0]+1
     return not selfIntersects(snake, nextHeadPosition)
 
-def selfIntersects(snake, nextHeadPosition):
-    """Checks whether the snake would collide with itself or not.
 
-    Parameters
-    ----------
-    snake : array of 2-dimensional arrays 
-        Position of each snake bodypart on the board.
-        3 <= snake length <= 7
-    nextHeadPosition : 2-dimensional array of integers
-        Position the head of the snake is attempting to take.
-    """
-
-    for bodyPartPosition in snake[1:-1]:
-        if nextHeadPosition == bodyPartPosition:
-            return True
-    
-    return False
-
+print("numberOfAvailableDifferentPaths = ")    
+print(numberOfAvailableDifferentPaths([2, 3], [[0,2], [0,1], [0,0], [1,0], [1,1], [1,2]], 10))
